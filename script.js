ab0676c27920627a20995efb042199a45c00bade
rohitjainfsl <rohit@fullstacklearning.com>
2025-01-09 18:09:07 +0530

    handled load more button

--- script.js
+++ script.js
@@ -1,29 +1,64 @@
 const wrapper = document.querySelector("#wrapper");
-async function pokimon(url){
-    const response = await fetch(url);
-    const result = await response.json();
-    return result;
-    // console.log(result);
-}
+const loadMoreBtn = document.querySelector("#loadMore");
+const baseURL = "https://pokeapi.co/api/v2/pokemon";
+
+let limit = 20;
+let count = 0;
 
 const image = [];
 const saveUrl = [];
-window.addEventListener("load",async() =>{
-    const saveData =  await pokimon("https://pokeapi.co/api/v2/pokemon?limit=20&offset=0");
-    // console.log(saveData)
 
-    saveData.results.forEach(data => {
-       const final =  pokimon(data.url);
-       saveUrl.push(final);
-    //    console.log(saveUrl);
-        
-    });
-    const data = await Promise.all(saveUrl);
-    console.log(data);
-
-
-
+window.addEventListener("load", async () => {
+  const finalData = await getData(
+    `${baseURL}?limit=${limit}&offset=${limit * count}`
+  );
+  displayData(finalData);
 });
 
+loadMoreBtn.addEventListener("click", async () => {
+  count++;
+  const finalData = await getData(
+    `${baseURL}?limit=${limit}&offset=${limit * count}`
+  );
+  displayData(finalData);
+});
 
+async function pokimon(url) {
+  const response = await fetch(url);
+  const result = await response.json();
+  return result;
+}
 
+function displayData(data) {
+  wrapper.innerHTML = "";
+  const pokemonDiv = document.createElement("div");
+  pokemonDiv.classList.add("pokemonDiv");
+
+  data.forEach((obj) => {
+    const parent = document.createElement("div");
+    const image = document.createElement("img");
+    const name = document.createElement("h3");
+
+    parent.classList.add("parent");
+    name.classList.add("name");
+
+    image.src = obj.sprites.other.dream_world.front_default;
+    name.innerText = obj.name;
+
+    parent.append(image, name);
+    pokemonDiv.append(parent);
+  });
+
+  wrapper.append(pokemonDiv);
+}
+
+async function getData(url) {
+  const saveData = await pokimon(url);
+
+  saveData.results.forEach((data) => {
+    const final = pokimon(data.url);
+    saveUrl.push(final);
+  });
+  const data = await Promise.all(saveUrl);
+  return data;
+}
